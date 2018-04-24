131df123f0929f6d090ec2c04644b1cccb6b4fbb
PowerpufLuv <kostya.anopriev.99@mail.ru>
2018-04-24 08:37:44 +0300

    tri commit

--- laba_1_0/Program.cs
+++ laba_1_0/Program.cs
@@ -9,8 +9,19 @@ namespace laba_1_0
 {
     class Program
     {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Chet schet = new Chet();
+            Console.WriteLine(schet);
+            //schet.Name = "Nomber one";
+            Chet schet2 = new Chet("Two", 150, 2.1);
+            Console.WriteLine(schet2);
+            Console.ReadKey();
         }
+
     }
 }
